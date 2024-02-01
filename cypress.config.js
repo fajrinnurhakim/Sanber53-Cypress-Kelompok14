9234036523014b7b8007af3f2fde54c6a9b5c18a
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-02-01 10:50:24 +0700

    cypresss(config): test config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        env: {
+            BASE_URL: "https://magento.softwaretestingboard.com/",
+        },
+        viewportWidth: 1200,
+        viewportHeight: 800,
+        defaultCommandTimeout: 5000,
+        requestTimeout: 12000,
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 });
---
+++ cypress/e2e/2-magento-web-test/test.cy.js
@@ -0,0 +1,9 @@
+describe("example to-do app", () => {
+    beforeEach(() => {
+        cy.visit(Cypress.env("BASE_URL"));
+    });
+
+    it("displays magento", () => {
+        // test
+    });
+});
