258114fe5762813a7dce19ce0404fca9f6e98176
mchafrizal <moch.afrizal00@gmail.com>
2024-02-05 19:09:18 +0700

    change login test

--- cypress/e2e/2-magento-web-test/login-test.cy.js
+++ cypress/e2e/2-magento-web-test/login-test.cy.js
@@ -1,10 +1,33 @@
 describe('login test', () => {
-  it('fail login', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
-    cy.get('#email').type('test_1@yop.com')
-    cy.get('#pass').type(63636)
+  beforeEach(()=> {
+     cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')      
+ })
+  it('positive login', () => {
+    cy.get('#email').type('mafrizal@g.com')
+    cy.get('#pass').type('Cek081330')
+    cy.get('.action.login.primary').click()
+ })
+  it('negative login password', () => {
+    cy.get('#email').type('mafrizal@g.com')
+    cy.get('#pass').type('89829')
+    cy.get('.action.login.primary').click()
+    cy.get('.note')
+ })
+  it('negative login email note', () => {
+    cy.get('#email').type('mafri@g.com')
+    cy.get('#pass').type('89829')
+    cy.get('.action.login.primary').click()
+    cy.get('.note')
+  })
+  it('negative login message-error', () => {
+    cy.get('#email').type('mafri@g.com')
+    cy.get('#pass').type('89829')
     cy.get('.action.login.primary').click()
     cy.get('.message-error')
-
-   })
+  })
+   it('negative login', () => {
+    cy.get('#email').type('mafrizal@g.com')
+    cy.get('#pass').type('Cek0')
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > .secondary > .action > span').click();
+  })
 })
