b03b7a7a8ee3d4027d300228bd08efe7a1591e47
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-03-20 22:04:40 +0700

    workflow CI/CD

--- cypress/e2e/2-magento-web-test/choose-product.cy.js
+++ cypress/e2e/2-magento-web-test/choose-product.cy.js
@@ -5,10 +5,16 @@ describe("Choose Product", () => {
         cy.visit(Cypress.env("BASE_URL"));
     });
 
-    it("Choose Product - With Login", () => {
+    it("Choose Product - Without Login", () => {
+        // choose product
+        chooseProduct.choose();
+    });
+    it.only("Choose Product - With Login", () => {
         cy.get('.authorization-link > a:contains("Sign In")').first().click();
         // login with command
-        cy.loginCommand("rarafebbyy123@gmail.com", "Admin123!");
+        cy.get("#email").type("rarafebbyy123@gmail.com");
+        cy.get("#pass").type("Admin123!");
+        cy.get(".action.login.primary").click();
         // choose product
         chooseProduct.choose();
     });
