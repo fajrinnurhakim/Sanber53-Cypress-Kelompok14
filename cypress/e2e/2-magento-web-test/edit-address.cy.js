2c1209eefcb6bada6b292a37c4e331b44eb79b38
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-03-20 20:19:15 +0700

    workflow

--- cypress/e2e/2-magento-web-test/edit-address.cy.js
+++ cypress/e2e/2-magento-web-test/edit-address.cy.js
@@ -6,34 +6,34 @@ describe("Edit Address", () => {
         cy.visit(Cypress.env("BASE_URL"));
     });
 
-    it("Edit Billing Address - Success", () => {
-        cy.get('.authorization-link > a:contains("Sign In")').first().click();
-        // login with command
-        cy.loginCommand("fajrin123@gmail.com", "Fajrin123!");
-        // customer menu
-        cy.wait(2000);
-        cy.get(".customer-name").first().click();
-        cy.get(".customer-menu > .header.links > li:first-child > a")
-            .first()
-            .click();
-        //edit billing address
-        cy.get(".box-billing-address > .box-actions > .action > span").click();
-        cy.get("#firstname").clear().type(addressData["firstname"]);
-        cy.get("#lastname").clear().type(addressData["lastname"]);
-        cy.get("#company").clear().type(addressData["company"]);
-        cy.get("#telephone").clear().type(addressData["telephone"]);
-        cy.get("#street_1").clear().type(addressData["street_1"]);
-        cy.get("#city").clear().type(addressData["city"]);
-        cy.get("#country").select(addressData["country"]);
-        cy.get("#region").clear().type(addressData["region"]);
-        cy.get("#zip").clear().type(addressData["zip"]);
-        //button save
-        cy.get(
-            "#form-validate > .actions-toolbar > div.primary > .action"
-        ).click();
-        // verify success
-        editAddress.verifySuccess();
-    });
+    // it("Edit Billing Address - Success", () => {
+    //     cy.get('.authorization-link > a:contains("Sign In")').first().click();
+    //     // login with command
+    //     cy.loginCommand("fajrin123@gmail.com", "Fajrin123!");
+    //     // customer menu
+    //     cy.wait(2000);
+    //     cy.get(".customer-name").first().click();
+    //     cy.get(".customer-menu > .header.links > li:first-child > a")
+    //         .first()
+    //         .click();
+    //     //edit billing address
+    //     cy.get(".box-billing-address > .box-actions > .action > span").click();
+    //     cy.get("#firstname").clear().type(addressData["firstname"]);
+    //     cy.get("#lastname").clear().type(addressData["lastname"]);
+    //     cy.get("#company").clear().type(addressData["company"]);
+    //     cy.get("#telephone").clear().type(addressData["telephone"]);
+    //     cy.get("#street_1").clear().type(addressData["street_1"]);
+    //     cy.get("#city").clear().type(addressData["city"]);
+    //     cy.get("#country").select(addressData["country"]);
+    //     cy.get("#region").clear().type(addressData["region"]);
+    //     cy.get("#zip").clear().type(addressData["zip"]);
+    //     //button save
+    //     cy.get(
+    //         "#form-validate > .actions-toolbar > div.primary > .action"
+    //     ).click();
+    //     // verify success
+    //     editAddress.verifySuccess();
+    // });
 
     it("Edit Billing Address - Required Field", () => {
         cy.get('.authorization-link > a:contains("Sign In")').first().click();
